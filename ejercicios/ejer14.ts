78dab70dc8203c836554a4c138ba882b633153ca
uzyfabbargama <uzielgamarra733@gmail.com>
2026-06-09 10:23:17 -0300

    Implement product calculation using variables

---
+++ ejercicios/ejer14.ts
@@ -0,0 +1,13 @@
+//14: Crea un programa que calcule el producto de los números 121 y 132, usando variables.
+let a: number = 11
+let factor1a: number = 11
+
+let b: number = 11
+let factor1b: number = 10
+let factor2b: number = 2
+
+let ansfactor: number = factor1a + factor1b + factor2b
+
+let a: number = ansfactor * a
+
+console.log(`${a}`)
