f965f84c6b70ea0125b66d7f36bf79bafc0ebaed
uzyfabbargama <uzielgamarra733@gmail.com>
2026-06-09 10:43:29 -0300

    Implement product calculation for given numbers
    Calculate the product of numbers 87 and 94 using variables.

---
+++ ejercicios/ejer18.ts
@@ -0,0 +1,10 @@
+//18: Crea un programa que calcule el producto de los números 87 y 94, usando variables llamadas "numero1" y "numero2".
+//94 → 47
+//87 → 47+40
+let a: number = 47
+let b: number = 40
+let c: number = 47*47
+let d: number = 47*40
+let e: number = c+d
+
+consol.log(`${e}`)
