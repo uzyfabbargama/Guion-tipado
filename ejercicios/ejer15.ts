81dd0c4d66f7c462a2dafe31e26da879b9080e5c
uzyfabbargama <uzielgamarra733@gmail.com>
2026-06-09 10:34:30 -0300

    Correct sum calculation in ejer15.ts
    Fix variable declarations and log the sum of a and b.

---
+++ ejercicios/ejer15.ts
@@ -0,0 +1,8 @@
+//15: Crea un programa que calcule la suma de 285 y 1396, usando variables.
+let a: num =    0 +   0 + 256 +   0 +  0 +  0 + 16 + 8 + 4 + 0 + 1
+let b: num = 1024 +   0 + 256 +   0 + 64 + 32 + 16 + 0 + 4 + 0 + 0
+let c: num = 1024 + 512 +   0 + 128 +  0 +  0 + 0 + 0 + 0 + 0 + 1 
+
+conole.log(`${a+b}`)
+
+conole.log(`${c}`)
