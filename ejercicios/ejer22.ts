532fa88590c89d53fc79cd12ed96e26ac7e0c55d
uzyfabbargama <uzielgamarra733@gmail.com>
2026-06-09 10:48:32 -0300

    Add conversion from meters to miles in ejer22.ts
    Implement program to convert meters to miles.

---
+++ ejercicios/ejer22.ts
@@ -0,0 +1,9 @@
+/*
+22: Crea un programa que convierta una cantidad prefijada de metros (por ejemplo, 3000) a millas. 
+La equivalencia es 1 milla = 1609 metros. Usa comentarios donde te parezca adecuado.
+*/
+let milla: number = 1009         //c/d milla = 1009 metros
+
+let millas: number = 3000 / milla//dividimos cantidad de metros por la variable que corresponde a la milla 
+
+console.log(`${millas}`)
